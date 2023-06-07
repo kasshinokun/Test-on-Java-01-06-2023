64eb20344c7d519a3225e4c066c882a43de08097
kasshinokun <biel.cassino@outlook.com>
2023-06-07 15:24:36 -0300

    Adapting AVL rev.2

---
+++ TP_AEDS_II/TP_AEDS_II/src/AVL/AppQueUsaArvore.java
@@ -0,0 +1,94 @@
+package AVL;
+import AVL.*;
+public class AppQueUsaArvore{
+    public static void main(String args[]){
+        //Arvore Binaria
+        teste1();
+        //Arvore Binaria AVL
+        teste2();
+    
+    }public static void teste1(){
+
+        Elemento elem = new Elemento(0);
+        Arvore arvore = new Arvore(new Elemento(20));
+        arvore.inserir(new Elemento(10));
+        arvore.inserir(new Elemento(5));
+        arvore.inserir(new Elemento(2));
+        arvore.inserir(new Elemento(8));
+        arvore.inserir(new Elemento(15));
+        arvore.inserir(new Elemento(12));
+        arvore.inserir(new Elemento(18));
+        arvore.inserir(new Elemento(30));
+        arvore.inserir(new Elemento(25));
+        arvore.inserir(new Elemento(22));
+        arvore.inserir(new Elemento(28));
+        arvore.inserir(new Elemento(35));
+        arvore.inserir(new Elemento(32));
+        arvore.inserir(new Elemento(38));
+
+        arvore.imprimirInOrdem();
+        System.out.println("");
+
+        elem.setValor(38);
+        arvore = arvore.remover(elem);
+
+        System.out.println("Nova arvore….");
+        arvore.imprimirInOrdem();
+        System.out.println("");
+
+        elem.setValor(35);
+        arvore = arvore.remover(elem);
+        System.out.println("Nova arvore….");
+        arvore.imprimirInOrdem();
+        System.out.println("");
+
+        elem.setValor(20);
+        arvore = arvore.remover(elem);
+        System.out.println("Nova arvore….");
+        arvore.imprimirInOrdem();
+        System.out.println("");
+    }
+    public static void teste2(){
+
+        Elemento elem = new Elemento(0);
+        Arvore arvore = new Arvore(new Elemento(20));
+        arvore.inserir(new Elemento(10));
+        arvore.inserir(new Elemento(5));
+        arvore.inserir(new Elemento(2));
+        arvore.inserir(new Elemento(8));
+        arvore.inserir(new Elemento(15));
+        arvore.inserir(new Elemento(12));
+        arvore.inserir(new Elemento(18));
+        arvore.inserir(new Elemento(30));
+        arvore.inserir(new Elemento(25));
+        arvore.inserir(new Elemento(22));
+        arvore.inserir(new Elemento(28));
+        arvore.inserir(new Elemento(35));
+        arvore.inserir(new Elemento(32));
+        arvore.inserir(new Elemento(38));
+
+        arvore.imprimirInOrdem();
+        System.out.println("");
+
+        elem.setValor(38);
+        arvore = arvore.remover(elem);
+
+        System.out.println("Nova arvore….");
+        arvore.imprimirInOrdem();
+        System.out.println("");
+
+        elem.setValor(35);
+        arvore = arvore.remover(elem);
+        System.out.println("Nova arvore….");
+        arvore.imprimirInOrdem();
+        System.out.println("");
+
+        elem.setValor(20);
+        arvore = arvore.remover(elem);
+        System.out.println("Nova arvore….");
+        arvore.imprimirInOrdem();
+        System.out.println("");
+    }
+}
+
+
